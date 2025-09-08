9e28b1bbc072adc499832c80d83fcc81426ab7af
shaitan <shaitanaleksender@gmail.com>
2025-09-08 16:58:50 +0200

    Enforce minimum parallelism validation in PipelineBuilder.

--- FiftyOne.Pipeline.Core/FlowElements/PipelineBuilder.cs
+++ FiftyOne.Pipeline.Core/FlowElements/PipelineBuilder.cs
@@ -129,7 +129,7 @@ namespace FiftyOne.Pipeline.Core.FlowElements
                     new ParallelOptions()
                     {
                         MaxDegreeOfParallelism =
-                            Math.Max(1, Environment.ProcessorCount / 2) 
+                            Math.Max(1, Environment.ProcessorCount / 2)// 0 is not allowed.
                     },
                     (elementOptions, state, index)  =>
                 {
