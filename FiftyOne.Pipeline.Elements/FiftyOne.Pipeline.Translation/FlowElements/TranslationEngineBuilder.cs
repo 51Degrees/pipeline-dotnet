604fe63624b62efc1ba3d9cc41721be401ccc7a2
Ben51Degrees <ben@51degrees.com>
2026-03-17 12:02:34 +0000

    CLEANUP: Cleaned up final bits from translation engine work.

--- FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.Translation/FlowElements/TranslationEngineBuilder.cs
+++ FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.Translation/FlowElements/TranslationEngineBuilder.cs
@@ -1,4 +1,26 @@
-﻿using FiftyOne.Pipeline.Core.Data;
+﻿/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2026 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence
+ * (EUPL) v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+using FiftyOne.Pipeline.Core.Data;
 using FiftyOne.Pipeline.Core.FlowElements;
 using FiftyOne.Pipeline.Translation.Data;
 using Microsoft.Extensions.Logging;
@@ -41,7 +63,16 @@ namespace FiftyOne.Pipeline.Translation.FlowElements
         /// for a value.
         /// </summary>
         private MissingTranslationBehaviour _behaviour;
-        private string _fixedLanguage = null;
+
+        /// <summary>
+        /// Optional fixed language for the translation engine to translate to.
+        /// </summary>
+        private string _fixedLanguage;
+
+        /// <summary>
+        /// Logger passed to the <see cref="TranslationData"/> instances
+        /// created by the translation engine.
+        /// </summary>
         private readonly ILogger<TranslationData> _dataLogger;
 
         /// <summary>
@@ -54,6 +85,7 @@ namespace FiftyOne.Pipeline.Translation.FlowElements
             _translationProperties = new List<TranslationProperty>();
             _sources = new List<FileInfo>();
             _dataLogger = _loggerFactory.CreateLogger<TranslationData>();
+            _fixedLanguage = null;
             _behaviour = MissingTranslationBehaviour.Original;
         }
 
